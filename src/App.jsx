4bd1f8a6781b36879bd44b26406913db1b3aa143
Nil Farras <farrasnil@gmail.com>
2024-02-17 16:38:43 +0100

    execercies from 1.6 to 1.11 done! exercicies from 1.12 to 1.14 dont do it!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
 import { useState } from "react";
+import { StatisticLine} from "./Statistics.jsx";
 
 const App = () => {
-  // save each button click in its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = (good + neutral + bad);
+  const average = (good - bad) / all;
+  const positive = (good * 100) / all;
 
-  return <div>code here</div>;
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <button onClick={() => {setGood(good +1)}}>good</button>
+      <button onClick={() => {setNeutral(neutral +1)}}>neutral</button>
+      <button onClick={() => {setBad(bad +1)}}>bad</button>
+
+      <h1>statistics</h1>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={average} />
+            <StatisticLine text="positive" value={positive + "%"}/>
+          </tbody>
+        </table>
+        <h1>Anecdote of the day</h1>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Statistics.jsx
@@ -0,0 +1,9 @@
+export const StatisticLine = ({text, value}) => {
+
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    )
+}
